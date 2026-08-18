8502c67ee2692463adcbc5a2110c66ff5882a74b
JoshuaJoseFelix <162275395+JoshuaJoseFelix@users.noreply.github.com>
2026-08-18 19:21:14 +0530

    Time: 8 ms (49.56%) | Memory: 30 MB (70.48%) - LeetSync

--- 143-reorder-list/reorder-list.py
+++ 143-reorder-list/reorder-list.py
@@ -1,36 +1,24 @@
 class Solution(object):
     def reorderList(self, head):
-        if not head or not head.next:
-            return
-
-        # 1. Find the middle
-        slow, fast = head, head.next
+        slow,fast=head,head.next
 
         while fast and fast.next:
-            slow = slow.next
-            fast = fast.next.next
-
-        # 2. Split the list
-        second = slow.next
-        slow.next = None
-
-        # 3. Reverse the second half
-        prev = None
+            slow=slow.next
+            fast=fast.next.next    
+            
+            
+            
+        second=slow.next
+        prev = slow.next=None
         while second:
-            tmp = second.next
+            tmp=second.next
             second.next = prev
-            prev = second
+            prev = second 
             second = tmp
 
-        # 4. Merge the two halves
-        first, second = head, prev
-
+        first,second = head,prev
         while second:
-            tmp1 = first.next
-            tmp2 = second.next
-
+            tmp1,tmp2 = first.next,second.next
             first.next = second
-            second.next = tmp1
-
-            first = tmp1
-            second = tmp2
+            second.next =tmp1
+            first,second = tmp1,tmp2
